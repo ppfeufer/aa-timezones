9cca530c9a267b2e7a48c782b29f25be6211d7f4
Peter Pfeufer <info@ppfeufer.de>
2025-10-23 16:45:01 +0200

    [FIX] Use translated name as menu item and header

--- timezones/__init__.py
+++ timezones/__init__.py
@@ -6,4 +6,5 @@ Application init
 from django.utils.translation import gettext_lazy as _
 
 __version__ = "2.4.2"
-__title__ = _("Time Zones")
+__title__ = "Time Zones"
+__title_translated__ = _("Time Zones")
