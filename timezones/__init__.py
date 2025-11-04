8f878bb083c1178d8efc2818372a4eb3c4a0f973
Peter Pfeufer <info@ppfeufer.de>
2025-11-04 15:36:34 +0100

    [RELEASE] v2.4.3
    ## [2.4.3] - 2025-11-04
    
    ### Fixed
    
    - Use translated name as menu item and header
    
    ### Changed
    
    - Translations updated

--- timezones/__init__.py
+++ timezones/__init__.py
@@ -5,6 +5,6 @@ Application init
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "2.4.2"
+__version__ = "2.4.3"
 __title__ = "Time Zones"
 __title_translated__ = _("Time Zones")
