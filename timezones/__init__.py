5736b5851a13d99eaa3435070974a7c323abc5a4
Peter Pfeufer <info@ppfeufer.de>
2026-01-05 23:39:00 +0100

    [RELEASE] v2.4.4
    ## [2.4.4] - 2026-01-05
    
    ### Changed
    
    - Translations updated

--- timezones/__init__.py
+++ timezones/__init__.py
@@ -5,6 +5,6 @@ Application init
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "2.4.3"
+__version__ = "2.4.4"
 __title__ = "Time Zones"
 __title_translated__ = _("Time Zones")
