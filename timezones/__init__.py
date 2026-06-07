ddf5af73a47d42ddd872492913b268681cb7bc98
Peter Pfeufer <info@ppfeufer.de>
2026-06-07 15:25:12 +0200

    [RELEASE] v3.0.0
    ## [3.0.0] - 2026-06-07
    
    > [!IMPORTANT]
    >
    > **This version needs Alliance Auth v5!**
    >
    > Please make sure to update your Alliance Auth instance **before** you install this
    > version, otherwise an update to Alliance Auth will be pulled in unsupervised.
    
    ### Removed
    
    - Support for Alliance Auth v4

--- timezones/__init__.py
+++ timezones/__init__.py
@@ -5,6 +5,6 @@ Application init
 # Django
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "2.6.2"
+__version__ = "3.0.0"
 __title__ = "Time Zones"
 __title_translated__ = _("Time Zones")
